90c2586d9e8661e42918fc697de6d9e8738eadb3
unknown <arifoglutarik@gmail.com>
2021-09-21 18:27:41 +0300

    Added new student object, Removed unused code

--- SchoolApi/Controllers/SchoolController.cs
+++ SchoolApi/Controllers/SchoolController.cs
@@ -11,5 +11,17 @@ namespace SchoolApi.Controllers
     [ApiController]
     public class SchoolController : ControllerBase
     {
+        private static List<StudentModel> studentList = new List<StudentModel>()
+        {
+            new StudentModel(){Id=1,CourseId=100,Name="John",Surname="Doe",Gender='M'},
+            new StudentModel(){Id=2,CourseId=100,Name="Liza",Surname="Koter",Gender='F'},
+            new StudentModel(){Id=3,CourseId=200,Name="Fin",Surname="Olemna",Gender='F'},
+            new StudentModel(){Id=4,CourseId=300,Name="Eve",Surname="Lesto",Gender='F'},
+            new StudentModel(){Id=5,CourseId=320,Name="Abraham",Surname="Lincoln",Gender='M'},
+            new StudentModel(){Id=6,CourseId=320,Name="George",Surname="Gratt",Gender='M'},
+            new StudentModel(){Id=7,CourseId=320,Name="Ralph",Surname="Sutre",Gender='M'},
+            new StudentModel(){Id=8,CourseId=400,Name="Daniella",Surname="Ekto",Gender='F'}
+        };
+
     }
 }
---
+++ SchoolApi/StudentModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApi
+{
+    public class StudentModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Name should be between 2 and 20 characters")]
+        public string Name { get; set; }
+
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Surname should be between 2 and 20 characters")]
+        public string Surname { get; set; }
+
+        [Required]
+        [RegularExpression("(M|W)", ErrorMessage = "Only E or W is allowed")]
+        public char Gender { get; set; }
+    }
+}
